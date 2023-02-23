5e97d57c48c8804dff1f5cc81671a69d16a2a2d9
GJ Tiquia <gershom.tiquia@gmail.com>
2023-02-24 01:19:31 +0800

    added typescript implementation

--- apps/clickme/script.ts
+++ apps/clickme/script.ts
@@ -1,28 +1,33 @@
-"use strict";
-function init() {
+function init() : void {
     setCountText("0");
     addOnClickEventListener();
 }
-function addOnClickEventListener() {
+
+function addOnClickEventListener() : void {
     const button = document.getElementById("button");
     if (button != null)
         button.addEventListener("click", onButtonClick);
 }
-function getCountNumber() {
+
+function getCountNumber() : number {
     const countElement = document.getElementById("count");
     if (countElement != null)
         return parseInt(countElement.innerHTML);
-    console.error("script.getCountNumber(): countElement null!");
+
+    console.error("script.getCountNumber(): countElement null!")
     return -1;
 }
-function setCountText(text) {
+
+function setCountText(text : string) : void {
     const countElement = document.getElementById("count");
     if (countElement != null)
         countElement.innerHTML = text;
     else
-        console.error("script.setCountText(): countElement null!");
+        console.error("script.setCountText(): countElement null!")
 }
-function onButtonClick() {
+
+function onButtonClick() : void {
     setCountText((getCountNumber() + 1).toString());
 }
+
 init();
