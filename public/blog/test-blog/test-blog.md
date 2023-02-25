10a7cb81482d3697c01b21a4fbf9e36a7d4d3766
GJ Tiquia <gershom.tiquia@gmail.com>
2023-02-26 01:15:07 +0800

    tried generating html file from md and injecting it during runtime

---
+++ public/blog/test-blog/test-blog.md
@@ -0,0 +1,14 @@
+# Some title for a test blog
+
+## Introduction
+
+Hello! This is a paragraph.  
+This is the second line of the paragraph.
+
+```C#
+string someString = "This is a C# string";
+```
+
+## Conclusion
+
+It works!
---
+++ public/js/injectHtml.js
@@ -0,0 +1,7 @@
+"use strict";
+const container = document.getElementById('blog-contents-container');
+if (container != null) {
+    fetch("./test-blog.html")
+        .then(response => response.text())
+        .then(text => container.innerHTML = text);
+}
