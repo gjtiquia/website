df63d14fe7355a7621c4843104e54aa6d3f1480a
GJ Tiquia <gershom.tiquia@gmail.com>
2023-02-25 23:53:21 +0800

    added Showdown dependency for reading md in runtime

---
+++ src/testBlog.ts
@@ -0,0 +1,10 @@
+// showdown is from the Showdown CDN script
+const converter = new showdown.Converter();
+const text = '# hello, markdown!';
+const html = converter.makeHtml(text);
+console.log(html);
+
+const blogContentsContainer = document.getElementById("blog-contents-container");
+if (blogContentsContainer != null) {
+    blogContentsContainer.innerHTML = html;
+}
