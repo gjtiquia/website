2a0a158df3ab983a290925cc5bbdc4e643e61b93
GJ Tiquia <gershom.tiquia@gmail.com>
2025-07-13 13:57:15 -0400

    feat: change color theme to rose-pine-dawn

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                base: "#232136",
+                surface: "#2a273f",
+                overlay: "#393552",
+                muted: "#6e6a86",
+                subtle: "#908caa",
+                text: "#e0def4",
+                love: "#eb6f92",
+                gold: "#f6c177",
+                rose: "#ebbcba",
+                pine: "#31748f",
+                foam: "#9ccfd8",
+                iris: "#c4a7e7",
+                highlightLow: "#21202e",
+                highlightMed: "#403d52",
+                highlightHigh: "#524f67",
+            },
+        },
     },
     plugins: [],
 };
